33e44aa78a76654dd909b5c0a6abd5ec081418d0
PhamTheHuy <22130104@st.hcmuaf.edu.vn>
2025-05-19 06:03:57 +0700

    khởi tạo hàm và cấu trúc cơ bản cho giao diện tìm kiếm

---
+++ NMCNPM_Movie/app/src/main/java/com/example/nmcnpm_movie/ui/category/ListTypeItemScreen.kt
@@ -0,0 +1,46 @@
+package com.example.movie.ui.category
+
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.example.movie.ui.movie.CardMovieSearch
+
+
+@Composable
+fun MovieTypeScreen(slug:String,type:String,
+                    viewModel: TypeMovieViewmodel = hiltViewModel(),
+                    navController: NavController
+){
+
+
+    val moviePagingItems = viewModel.searchMoviePagingFlow.collectAsLazyPagingItems()
+
+
+    LaunchedEffect(Unit){
+        Log.d("search","$type fsd $slug")
+        viewModel.setQuery(type,slug)
+    }
+
+
+
+
+
+}
